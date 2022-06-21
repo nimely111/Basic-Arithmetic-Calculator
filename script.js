383f77a34a39936cd1027050dd11b96e90559557
nimely111 <samuelehnimely@gmail.com>
2022-06-21 09:39:10 +0000

    addition and subtration are now working properly in the alert and prompt

--- script.js
+++ script.js
@@ -4,5 +4,16 @@ const num1 = parseFloat(prompt('Enter Num1: '));//convert string to number
 const operator = prompt('Enter Operator to use: (+, -, *, /)');
 const num2 = parseFloat(prompt('Enter Num2: '));//convert string to number to perform calculation instead of concatenating
 
+let result = 0;
+if(isNaN(num1) || isNaN(num2)){
+    alert('Wrong input! Refresh and try again.');
+}else{
+    if(operator == '+'){
+        result = num1 + num2;
+    }else if(operator == '-'){
+        result = num1 - num2;
+    }
+    alert(num1 + operator + num2 + ' = ' + result)
+}
 
 
